c3cd6b75f55eb7037675ad7b0df78094aa490110
Ryan Wong <rjowong25@gmail.com>
2022-05-07 01:41:17 -0400

    Added docstrings to class and functions

--- simple_blockchain.py
+++ simple_blockchain.py
@@ -3,7 +3,42 @@ import json             #for block format
 from time import time   #for block timestamp
 
 class Blockchain(object):
+    """
+    A class to represent a blockchain.
+
+    ...
+
+    Attributes
+    ----------
+    chain : list of block
+        list of blocks
+    pending_transaction : list of transaction
+        list of transactions not yet approved and added to new block
+
+    Methods
+    -------
+    new_block(proof, previous_hash = None):
+        Builds a new block with pending transactions and adds it to the chain.
+    last_block():
+        Gets the most recent block added to the chain.
+    new_transactions(sender, receiver, amount):
+        Creates a new transaction and adds it to the pending transactions.
+    hash(block):
+        Creates a new hash given a block using SHA-256 encryption.
+    """
+    
     def __init__(self):
+        """
+        Constructs all necessary attributes for the blockchain object.
+
+        Parameters
+        ----------
+        chain : list of block
+            list of blocks
+        pending_transaction : list of transaction
+            list of transactions not yet approved and added to new block
+        """
+
         self.chain = []
         self.pending_transactions = []
 
@@ -11,6 +46,21 @@ class Blockchain(object):
 
     
     def new_block(self, proof, previous_hash = None):
+        """
+        Builds a new block with pending transactions and adds it to the chain.
+
+        Parameters
+        ----------
+        proof : int
+            proof of work value
+        previous_hash : str, optional
+            hash string of previously approved block
+
+        Returns
+        -------
+        block : JSON object with block properties
+        """
+        
         block = {
             'index': len(self.chain) + 1,
             'timestamp': time(),
@@ -26,10 +76,39 @@ class Blockchain(object):
     
     @property
     def last_block(self):
+        """
+        Gets the most recent block added to the chain.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        chain[-1] (block): The most recently added block
+        """
+
         return self.chain[-1]
 
     
     def new_transaction(self, sender, receiver, amount):
+        """
+        Creates a new transaction and adds it to the pending transactions.
+
+        Parameters
+        ----------
+        sender : str
+            string name of transaction sender
+        receiver : str
+            string name of transaction receiver
+        amount : str
+            string value of transaction
+
+        Returns
+        -------
+        last_block['index'] + 1 (int) : index of block this transaction will be added to
+        """
+        
         transaction = {
             'sender': sender,
             'receiver': receiver,
@@ -41,6 +120,19 @@ class Blockchain(object):
 
 
     def hash(self, block):
+        """
+        Creates a new hash given a block using SHA-256 encryption.
+
+        Parameters
+        ----------
+        block : JSON object
+            JSON object with block properties
+
+        Returns
+        -------
+        hex_hash (str) : hexidecimal string for new hash from block
+        """
+        
         stringify = json.dumps(block, sort_keys = True)
         block_unicode = stringify.encode()
 
